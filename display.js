ae0bd0c904bd04a6a8e1df24abc4c60d1558ded9
JoaquinJPB <jjpb2000@gmail.com>
2021-09-23 13:01:30 +0100

    Clear and ClearAll display

--- display.js
+++ display.js
@@ -2,13 +2,27 @@ class Display{
     constructor(displayPreviousValue, displayActualValue){
         this.displayPreviousValue = displayPreviousValue;
         this.displayActualValue = displayActualValue;
+        this.operatorType = undefined;
         this.calculator = new Calculator();
         this.actualValue = '';
         this.previousValue = '';
     }
 
+    clear(){
+        this.actualValue = this.actualValue.toString().slice(0,-1);
+        this.printValues();
+    }
+
+    clearAll(){
+        this.actualValue = '';
+        this.previousValue = '';
+        this.operatorType = undefined;
+        this.printValues();
+    }
+
     addNumber(number){
-        this.actualValue = number;
+        if(number === '.' && this.actualValue.includes('.')) return
+        this.actualValue = this.actualValue.toString() + number.toString();
         this.printValues();
     }
 
